710ebb1ab66a725b7d3eebbfcc06f834415c2f18
dwd <wenyi173@gmail.com>
2025-01-27 11:27:51 +0800

    udpate

--- meta_sleuth_client_test.go
+++ meta_sleuth_client_test.go
@@ -13,7 +13,7 @@ func TestMetaSleuthClient(t *testing.T) {
 
 func TestMetaSleuthClientGetAddressLabels(t *testing.T) {
 	c := NewDefaultMetaSleuthClient()
-	labels, err := c.GetAddressLabels(-3, "44W73kGYQgXCTNkGxUmHv8DDBPCxojBcX49uuKmbFc9U")
+	labels, err := c.GetAddressLabels(-3, "CS94dE2znqTYxcFtfdfpsEjkphBdsVZeVKhLoykMX7Ge")
 	if err != nil {
 		t.Fatal(err)
 	}
