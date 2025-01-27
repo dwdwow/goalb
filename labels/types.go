38233efaacd0feb276804e59c00a763865c9bba7
dwd <wenyi173@gmail.com>
2025-01-27 14:57:31 +0800

    udpate

--- labels/types.go
+++ labels/types.go
@@ -2,3 +2,13 @@ package labels
 
 type LabelName string
 type LabelID int
+
+type Label struct {
+	LabelName LabelName `json:"labelName" bson:"labelName"`
+	LabelID   LabelID   `json:"labelId" bson:"labelId"`
+}
+
+type AddressLabels struct {
+	Address string  `json:"address" bson:"address"`
+	Labels  []Label `json:"labels" bson:"labels"`
+}
