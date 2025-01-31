afc954eaedbb3514b982e42d8a5460bee09181f5
dwd <wenyi173@gmail.com>
2025-01-31 19:28:43 +0800

    update

--- labels/wallet.go
+++ labels/wallet.go
@@ -1,7 +1,8 @@
 package labels
 
 const (
-	HOT_WALLET  Label = "HOT_WALLET"
-	COLD_WALLET Label = "COLD_WALLET"
-	MULTISIG    Label = "MULTISIG"
+	HOT_WALLET         Label = "HOT_WALLET"
+	COLD_WALLET        Label = "COLD_WALLET"
+	MULTISIG           Label = "MULTISIG"
+	CEX_DEPOSIT_WALLET Label = "CEX_DEPOSIT_WALLET"
 )
